14ba2a215b0f674bd7ea51aee54f3ed16f0bffbb
unknown <caimingchao@tuhu.ad>
2018-05-02 16:48:04 +0800

    vue

---
+++ src/views/user/index.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="wrap">
+       {{mes}}
+  </div>
+</template>
+
+<script>
+ export default {
+     name: 'User',
+     data(){
+         return {
+             mes: 'test'
+         }
+     }
+ }   
+</script>
+
+
+<style scoped>
+   .wrap{
+       width: 300px;
+       height: 300px;
+       background: red;
+   }
+</style>
